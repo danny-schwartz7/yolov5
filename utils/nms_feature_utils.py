d26eeb07cbda13a8553f18d53198dab782321546
Ashish Jain <ashishjain87@gmail.com>
2021-06-02 18:15:51 -0700

    Addressed PR comments

--- utils/nms_feature_utils.py
+++ utils/nms_feature_utils.py
@@ -1,12 +1,12 @@
 import torch
 from typing import Tuple
 from typing import List
-import feature_utils
+import utils.feature_utils
 
 DEFAULT_BOX_LIMIT = 300
 DEFAULT_NUM_CHANNELS = 3
 
-def nms_predicted_bboxes_to_pixel_map(boxes: List[torch.Tensor], img_shape: Tuple[int, int],
+def nms_predicted_bboxes_to_pixel_map(boxes: List[torch.Tensor], img_shape: Tuple[int, int], num_channels = DEFAULT_NUM_CHANNELS,
                                   keep_top_n_boxes: int = DEFAULT_BOX_LIMIT) -> torch.Tensor:
     """
     Converts a tensor of bounding box information for a minibatch of examples into a pixel map for each class.
@@ -18,13 +18,13 @@ def nms_predicted_bboxes_to_pixel_map(boxes: List[torch.Tensor], img_shape: Tupl
         'features' has the following values, in this order: x, y, w, h, objectness, class_scores
         where class_scores is num_classes long, from 0 to the max class index.
     :param img_shape: the target output shape, a tuple of 2 integers - image width and image height.
+    :param num_channels: Number of channels in the image
     :param keep_top_n_boxes: An integer that controls the number of boxes used for the pixel map.
         Higher values are more accurate but slower.
     :return: A Tensor of pixel values between 0 and 1. Its shape will be [batch_size, nc, img_shape[0], img_shape[1]]
     """
 
-    numChannels = DEFAULT_NUM_CHANNELS # TODO: Change to class and get this value from constructor instead
-    numFeatures = numChannels + 5
+    numFeatures = num_channels + 5
 
     max_width_px = img_shape[0]
     max_height_px = img_shape[1]
@@ -33,7 +33,7 @@ def nms_predicted_bboxes_to_pixel_map(boxes: List[torch.Tensor], img_shape: Tupl
 
     numImages = len(boxes)
 
-    output = torch.zeros((numImages, numChannels, max_width_px, max_height_px)).to(device)
+    output = torch.zeros((numImages, num_channels, max_width_px, max_height_px)).to(device)
 
     for imageIndex in range(numImages):
         imageBoxes = boxes[imageIndex] 
